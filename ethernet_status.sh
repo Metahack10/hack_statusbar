43ae18c2937705cf5b8279fbc57f35597cc07615
Jorge Lopez <149315048+Metahack10@users.noreply.github.com>
2023-10-30 12:30:12 -0400

    Delete ethernet_status.sh

--- ethernet_status.sh
+++
@@ -1,4 +0,0 @@
-  GNU nano 6.2                                                                                 ethernet_status.sh                                                                                           
-#!/bin/bash
-
-echo   "$(/usr/sbin/ifconfig eth0 | grep 'inet ' | awk '{print $2}')    "
