9928e7b003aff0fd0b3cd6dceaa130fa4349acb8
Jorge Lopez <149315048+Metahack10@users.noreply.github.com>
2023-11-01 17:25:23 -0400

    Add files via upload

--- panels/puertos-en-uso.sh
+++ panels/puertos-en-uso.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-netstat -tulnp | grep "LISTEN" | awk '{print $4, $7}' | awk -F'/' '{print $1, $2}' | awk -F ' ' '{print $1,$3}' | sed 's/0.0.0.0://;s/::://' > /home/kali/.config/panels/puertos-abiertos.txt
+netstat -tunap |grep "LISTEN\|ESTABLISHED"|grep "tcp\|tcp6"|awk '{print $4, $6, $7}' > /home/kali/.config/panels/puertos-abiertos.txt
 
 listapuertos="/home/kali/.config/panels/puertos-abiertos.txt"
 separador=" : "
 while read -r lineaquelee
 do
     linea=$lineaquelee
-    puertos=$(echo $linea | awk -F ' ' '{print $1}')
-    servicio=$(echo $linea | awk '{print $2}')
-    total+="🚦️ "$puertos$separador$servicio" | "
+    puertos=$(echo $linea | grep -o ':[0-9]*'|grep -o '[0-9]*')
+    estado=$(echo $linea | awk -F ' ' '{print $2}')
+    servicio=$(echo $linea | grep -o '/[a-z]*'|grep -o '[a-z]*')
+    if [ "$estado" == "ESTABLISHED" ]; then
+        total+="🚦️ "$puertos$separador$servicio" | "
+    else
+        total+="🚥️ "$puertos$separador$servicio" | "
+    fi
 done < $listapuertos
 if [ -n "$total" ]; then
     echo "$total"
